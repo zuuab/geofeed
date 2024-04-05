28deca23b363ffcad86eb15564c680f726a01da3
zachheaton <zacharymartinheaton@gmail.com>
2024-04-05 11:35:36 -0600

    geofeed updates

--- geofeed_format.py
+++ geofeed_format.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
+import re
 
 with open('/Users/zachheaton/Desktop/work/geofeed/crap_format.txt', 'r') as file:
     lines = file.readlines()
     formatted_lines = []
     for line in lines:
-        components = line.strip().split(' - ')
+        components = re.split(r'\s{2,}', line.strip())
 
         ip_network = components[0]
-
         location_info = components[1].split(', ')
 
         reformatted_line = ','.join([ip_network] + location_info)
